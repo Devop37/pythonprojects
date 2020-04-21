505827a712ae841a1e17f2392eb737765f61d823
root <root@devx-python.ztek.local>
2020-04-21 01:44:33 -0400

    added new changes

---
+++ conditionals.py
@@ -0,0 +1,8 @@
+likes_pizza = "Yeah"
+
+if(likes_pizza == "Absolutely!"):
+    print("U R weird!")
+elif(likes_pizza == "Sort of"):
+    print("I guess U R ok")
+else:
+    print("U R normal.")
--- game.py
+++ game.py
@@ -1,3 +1,13 @@
 # Author: Diliano
 # Comment: d.y.73@mail.ru
 # A fun little python game
+name = input("What's your name?")
+age = int(input("What's your age?"))
+age_years = age+100
+likes_pizza = True
+
+print("Hi, " + name + "!")
+print("Your age is " + str(age) + " and in 100 years you are going to be in your " + str(age_years))
+
+if(likes_pizza == True):
+    print("Whoa!")
