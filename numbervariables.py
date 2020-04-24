4b4b0eeee11f1be902f7641214f8e7ac2b69897a
root <root@devx-python.ztek.local>
2020-04-24 15:39:43 -0400

    Variables

---
+++ numbervariables.py
@@ -0,0 +1,42 @@
+variable4=(((4**2)**2)-64)/8
+print(variable4)
+
+variable1=5
+variable1=variable1 ** 3
+print(variable1)
+
+a = 2
+a= ( a ** a / a + a )
+print("value of a: "+str(a))
+
+a = 20
+b = 10
+a = a/b
+print(a)        # 0.2
+
+a = a + 10
+print(a)
+
+# 10
+a = 40
+b = 20
+b = b - a
+print(b)
+
+# 11
+import math
+x = math.sqrt(81)
+print(x)
+print(math.sqrt(144))
+
+# 12
+print(math.sqrt(512/2))
+
+# 13
+a = 4
+b = 5
+c = math.sqrt((4**2) + (5**2))
+
+# c^2 = a^2 + b^2
+# c = sqrt(a^2 + b^2)
+print(c)
---
+++ variables.py
@@ -0,0 +1,38 @@
+name="Nina"
+print(name)
+
+# Integer values 1 2 3 4 ...
+an_int = 12
+# Float values  10.2  2.6  100.03  ...
+a_float = 2.3
+the_answer = an_int * a_float
+print(the_answer)
+
+blah_intiger = an_int
+blah_float = a_float
+the_answer = blah_intiger / blah_float
+
+print(the_answer)
+print(the_answer == an_int / a_float)
+if (the_answer == an_int / a_float):
+    print("YES!!!")
+
+print(3**3+3>=30)
+if(3**3+3>=30):
+    print(True)
+else:
+    print(False)
+
+is_happy=True
+if (is_happy==True):
+    print("is_happy!")
+
+if ("hello" !="Hello!"):
+    print("These are not equal")
+else:
+    print("These are equal")
+
+int1=1
+int2=2
+int3=100
+print(int1+int2 < int3-int1)
